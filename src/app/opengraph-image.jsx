97a0aa08df4c0daef924fec13c5cdcd74bf76fc7
Robin Willis <robincwillis@gmail.com>
2024-07-30 10:09:56 -0700

    second pass og images

--- src/app/opengraph-image.jsx
+++ src/app/opengraph-image.jsx
@@ -1,30 +1,30 @@
 import { ImageResponse } from "next/og";
 import Image from "next/image";
 
-import { getProject } from "@/lib/api/projects";
+import { getPage } from "@/lib/api/pages";
 
 // Route segment config
 export const runtime = "edge";
 
 export const size = {
-  width: 600,
+  width: 800,
   height: 800,
 };
 
 export const contentType = "image/jpg";
 
 export default async function OpenGraphImage({ params }) {
-  const project = await getProject(params.slug);
-  const { heroImage } = project;
+  const page = await getPage("info");
+  const lockup = page?.sectionsCollection?.items[0];
 
   return new ImageResponse(
     (
       <img
-        src={heroImage.url}
+        src={lockup.image.url}
         width={size.width}
         height={size.height}
         style={{
-          objectFit: "contain",
+          objectFit: "cover",
         }}
       />
     ),
